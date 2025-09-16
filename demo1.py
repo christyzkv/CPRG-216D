1cdd8d98ed6b92f39ed0f278731c69b883e527ea
christyzkv <christinaphamvuong@gmail.com>
2025-09-16 10:06:02 -0600

    mydemo.py

---
+++ demo1.py
@@ -0,0 +1,9 @@
+x = 3
+y = 4.5
+message = "hello"
+msg = 'hola'
+print = 5
+print(x)
+print(y)
+print(message)
+print(msg)
---
+++ mydemo.py
@@ -0,0 +1,16 @@
+a = 5
+b = 10
+c = 2
+d = (a+b-c)
+leaving_msg = "Goodbye!"
+respondent_msg = "how are you?"
+name = 'christina'
+msg = "bye"
+
+print(d)
+print(a)
+print(b)
+print(c)
+print(leaving_msg)
+print(respondent_msg)
+print(name)
